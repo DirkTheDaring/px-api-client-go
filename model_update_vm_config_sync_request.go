e098afb833d557f284203e3ba506e1e6bb418988
Dietmar Kling <baldur@email.de>
2023-12-18 10:52:03 +0100

    memory attribute is optional

--- model_update_vm_config_sync_request.go
+++ model_update_vm_config_sync_request.go
@@ -13,7 +13,6 @@ package pxapiflat
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 // checks if the UpdateVMConfigSyncRequest type satisfies the MappedNullable interface at compile time
@@ -224,7 +223,7 @@ type UpdateVMConfigSyncRequest struct {
 	// Specifies the QEMU machine type.
 	Machine *string `json:"machine,omitempty"`
 	// Memory
-	Memory int64 `json:"memory"`
+	Memory *int64 `json:"memory,omitempty"`
 	// Set maximum tolerated downtime (in seconds) for migrations.
 	MigrateDowntime *float32 `json:"migrate_downtime,omitempty"`
 	// Set maximum speed (in MB/s) for migrations. Value 0 is no limit.
@@ -603,15 +602,12 @@ type UpdateVMConfigSyncRequest struct {
 	Watchdog *string `json:"watchdog,omitempty"`
 }
 
-type _UpdateVMConfigSyncRequest UpdateVMConfigSyncRequest
-
 // NewUpdateVMConfigSyncRequest instantiates a new UpdateVMConfigSyncRequest object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewUpdateVMConfigSyncRequest(memory int64) *UpdateVMConfigSyncRequest {
+func NewUpdateVMConfigSyncRequest() *UpdateVMConfigSyncRequest {
 	this := UpdateVMConfigSyncRequest{}
-	this.Memory = memory
 	return &this
 }
 
@@ -3855,28 +3851,36 @@ func (o *UpdateVMConfigSyncRequest) SetMachine(v string) {
 	o.Machine = &v
 }
 
-// GetMemory returns the Memory field value
+// GetMemory returns the Memory field value if set, zero value otherwise.
 func (o *UpdateVMConfigSyncRequest) GetMemory() int64 {
-	if o == nil {
+	if o == nil || IsNil(o.Memory) {
 		var ret int64
 		return ret
 	}
-
-	return o.Memory
+	return *o.Memory
 }
 
-// GetMemoryOk returns a tuple with the Memory field value
+// GetMemoryOk returns a tuple with the Memory field value if set, nil otherwise
 // and a boolean to check if the value has been set.
 func (o *UpdateVMConfigSyncRequest) GetMemoryOk() (*int64, bool) {
-	if o == nil {
+	if o == nil || IsNil(o.Memory) {
 		return nil, false
 	}
-	return &o.Memory, true
+	return o.Memory, true
 }
 
-// SetMemory sets field value
+// HasMemory returns a boolean if a field has been set.
+func (o *UpdateVMConfigSyncRequest) HasMemory() bool {
+	if o != nil && !IsNil(o.Memory) {
+		return true
+	}
+
+	return false
+}
+
+// SetMemory gets a reference to the given int64 and assigns it to the Memory field.
 func (o *UpdateVMConfigSyncRequest) SetMemory(v int64) {
-	o.Memory = v
+	o.Memory = &v
 }
 
 // GetMigrateDowntime returns the MigrateDowntime field value if set, zero value otherwise.
@@ -10208,7 +10212,9 @@ func (o UpdateVMConfigSyncRequest) ToMap() (map[string]interface{}, error) {
 	if !IsNil(o.Machine) {
 		toSerialize["machine"] = o.Machine
 	}
-	toSerialize["memory"] = o.Memory
+	if !IsNil(o.Memory) {
+		toSerialize["memory"] = o.Memory
+	}
 	if !IsNil(o.MigrateDowntime) {
 		toSerialize["migrate_downtime"] = o.MigrateDowntime
 	}
@@ -10774,41 +10780,6 @@ func (o UpdateVMConfigSyncRequest) ToMap() (map[string]interface{}, error) {
 		toSerialize["watchdog"] = o.Watchdog
 	}
 	return toSerialize, nil
-}
-
-func (o *UpdateVMConfigSyncRequest) UnmarshalJSON(bytes []byte) (err error) {
-    // This validates that all required properties are included in the JSON object
-	// by unmarshalling the object into a generic map with string keys and checking
-	// that every required field exists as a key in the generic map.
-	requiredProperties := []string{
-		"memory",
-	}
-
-	allProperties := make(map[string]interface{})
-
-	err = json.Unmarshal(bytes, &allProperties)
-
-	if err != nil {
-		return err;
-	}
-
-	for _, requiredProperty := range(requiredProperties) {
-		if _, exists := allProperties[requiredProperty]; !exists {
-			return fmt.Errorf("no value given for required property %v", requiredProperty)
-		}
-	}
-
-	varUpdateVMConfigSyncRequest := _UpdateVMConfigSyncRequest{}
-
-	err = json.Unmarshal(bytes, &varUpdateVMConfigSyncRequest)
-
-	if err != nil {
-		return err
-	}
-
-	*o = UpdateVMConfigSyncRequest(varUpdateVMConfigSyncRequest)
-
-	return err
 }
 
 type NullableUpdateVMConfigSyncRequest struct {
